c4c17e1e1a18b74abc5ddcc92c4e1519b121de2f
Promise <promiseprince01@gmail.com>
2023-07-04 14:55:08 +0100

    created a folder for the frontend process of recieving payment

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const https = require('https');
+const axios = require('axios');
 
 
 const payStack = {
@@ -53,29 +54,39 @@ const payStack = {
   verifyPayment: async(req, res) => {
     try {
       const options = {
-        hostname: 'api.paystack.co',
+        hostname: 'https://api.paystack.co',
         port: 443,
-        path: '/transaction/verify/:775488184',
+        path: '/transaction/verify/:m42logswb9',
         method: 'GET',
         headers: {
           Authorization: process.env.PUBLIC_KEY
         }
       }
+
+      axios(options)
+      .then(response => {
+        console.log(response.data);
+        return res.status(200).json(response.data);
+      }).catch(function (error) {
+        console.log(error.message);
+        return res.status(500).json(error.message);
+      });
       
-      https.request(options, apiRes => {
-        let data = ''
+
+      // https.request(options, apiRes => {
+      //   let data = ''
       
-        apiRes.on('data', (chunk) => {
-          data += chunk
-        });
+      //   apiRes.on('data', (chunk) => {
+      //     data += chunk
+      //   });
       
-        apiRes.on('end', () => {
-          console.log(JSON.parse(data))
-          return res.status(200).json(data);
-        })
-      }).on('error', error => {
-        console.error(error)
-      })
+      //   apiRes.on('end', () => {
+      //     console.log(JSON.parse(data))
+      //     return res.status(200).json(data);
+      //   })
+      // }).on('error', error => {
+      //   console.error(error)
+      // })
     } catch (error) {
        // Handle any errors that occur during the request
        console.error(error);
