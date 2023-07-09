e042bcda598bdcfd762a2ee6a866fe457d45f081
Promise <promiseprince01@gmail.com>
2023-07-09 23:57:43 +0100

    updated the code for webhook

--- Controllers/webhook.js
+++ Controllers/webhook.js
@@ -6,16 +6,18 @@ const secret = process.env.SECRET_KEY;
 const webhook = (req, res) => {
   //validate event
   const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(req.body)).digest('hex');
+
   if (hash == req.headers['x-paystack-signature']) {
-  // Retrieve the request's body
-  const event = req.body;
-  // Do something with event 
-  if (event && event.event === 'transfer.success') {
-    return res.status(200).json({ message: 'Transfer successful' })
-  }
+    // Retrieve the request's body
+    const event = req.body;
+    // Do something with event
+    if (event && event.event === 'transfer.success') {
+      return res.status(200).json({ message: 'Transfer successful' })
+    }  
+    } 
   
   res.send(200);
-}}
+}
 
 module.exports = webhook;
 
