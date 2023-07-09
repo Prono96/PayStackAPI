f976ea57e9e06f6e3f4074ecdab61aa9d61022e6
Promise <promiseprince01@gmail.com>
2023-07-09 23:08:56 +0100

    updated the webhook controller

--- Controllers/webhook.js
+++ Controllers/webhook.js
@@ -8,13 +8,12 @@ const webhook = (req, res) => {
   const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(req.body)).digest('hex');
   if (hash == req.headers['x-paystack-signature']) {
   // Retrieve the request's body
-
-  
   const event = req.body;
   // Do something with event 
   if (event && event.event === 'transfer.success') {
     return res.status(200).json({ message: 'Transfer successful' })
   }
+  
   res.send(200);
 }}
 
